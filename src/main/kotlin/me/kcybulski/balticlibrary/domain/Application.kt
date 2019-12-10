b22ca281299f92deafd30965b1458f35e8991441
Krzysztof Cybulski <krzysztof.cybulski@allegro.pl>
2019-12-10 15:54:27 +0100

    Add entity annotation

--- src/main/kotlin/me/kcybulski/balticlibrary/domain/Application.kt
+++ src/main/kotlin/me/kcybulski/balticlibrary/domain/Application.kt
@@ -1,8 +1,9 @@
 package me.kcybulski.balticlibrary.domain
 
-import javax.persistence.GeneratedValue
+import javax.persistence.Entity
 import javax.persistence.Id
 
+@Entity
 data class Application(@Id val id: String, val name: String,
                        val description: String, val icon: String,
                        val schema: List<StepParameter<Any>>)
